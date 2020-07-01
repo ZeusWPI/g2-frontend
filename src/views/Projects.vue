6a54b75bf0f4817698f0e075a5a83b248733ebee
maartenvn <maartenvn.eu@gmail.com>
2020-07-01 09:22:03 +0200

    Add search field to projects page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,7 +1,11 @@
 <template>
     <v-container class="container--small">
         <!-- Action bar -->
-        <v-row justify="end">
+        <v-row justify="space-between">
+            <v-col>
+                <v-text-field v-model="search" label="Search for a project" append-icon="mdi-magnify" outlined dense />
+            </v-col>
+
             <v-col cols="auto">
                 <v-btn color="primary" depressed @click="openCreate">
                     Nieuw project
@@ -17,21 +21,22 @@
                 <project-card v-for="index of 8" :key="index" :loading="true" />
             </template>
 
+            <!-- No Data -->
+            <template v-else-if="projects.isSuccess() && projectsFiltered.length <= 0">
+                <v-col cols="12">
+                    <h2>No projects found</h2>
+                    There are no projects available for the given search parameters.
+                </v-col>
+            </template>
+
             <!-- Data -->
             <template v-else-if="projects.isSuccess()">
-                <project-card
-                    v-for="(project, index) of projects.data"
-                    :key="index"
-                    :project="project"
-                />
+                <project-card v-for="(project, index) of projectsFiltered" :key="index" :project="project" />
             </template>
 
             <!-- Error -->
             <template v-else-if="projects.isError()">
-                <error-placeholder
-                    :error="projects.error"
-                    :options="{ style: 'SECTION', displayFullPage: true }"
-                />
+                <error-placeholder :error="projects.error" :options="{ style: 'SECTION', displayFullPage: true }" />
             </template>
         </v-row>
     </v-container>
@@ -55,6 +60,20 @@ export default class ProjectsView extends Vue {
      * List with available projects
      */
     projects: EchoPromise<Project[]> = ProjectService.getAll();
+
+    /**
+     * Search field value.
+     */
+    search = "";
+
+    /**
+     * Get the list with projects, filtered on the given search string.
+     */
+    get projectsFiltered(): Project[] {
+        const projects = this.projects.data || [];
+
+        return projects.filter(project => project.name.includes(this.search));
+    }
 
     /**
      * Open the create projects modal.
