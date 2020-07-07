30d0fb0a383d468a1b3c6bf2c3ab06a143ba22cb
maartenvn <maartenvn.eu@gmail.com>
2020-07-07 09:50:49 +0200

    Add user badge & menu to navigation bar

--- src/components/layout/navigation/NavigationBar.vue
+++ src/components/layout/navigation/NavigationBar.vue
@@ -15,31 +15,77 @@
             <v-btn v-for="(link, index) in links" :key="index" :to="link.to" exact text>
                 {{ link.text }}
             </v-btn>
-
-            <!-- Authentication -->
-            <template>
-                <!-- Loading -->
-                <template v-if="user.isLoading()">
-                    <v-btn text>
-                        <v-skeleton-loader class="skeleton__user" width="100px" type="text" />
-                    </v-btn>
-                </template>
-
-                <!-- Logged in -->
-                <template v-else-if="user.isSuccess()">
-                    <v-btn text>
-                        {{ user.data.name }}
-                    </v-btn>
-                </template>
-
-                <!-- Not logged in -->
-                <template v-else-if="user.isError()">
-                    <v-btn text @click="openLogin">
-                        Login
-                    </v-btn>
-                </template>
-            </template>
         </v-toolbar-items>
+
+        <!-- Authentication -->
+        <div class="ml-3">
+            <!-- Loading -->
+            <template v-if="user.isLoading()">
+                <v-btn text>
+                    <user-avatar loading />
+                </v-btn>
+            </template>
+
+            <!-- Logged in -->
+            <template v-else-if="user.isSuccess()">
+                <v-menu offset-y>
+                    <!-- Button -->
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn v-bind="attrs" v-on="on" icon rounded>
+                            <user-avatar :user="user.data" />
+                        </v-btn>
+                    </template>
+
+                    <!-- Menu -->
+                    <v-list nav dense>
+                        <v-list-item>
+                            <!-- Avatar -->
+                            <v-list-item-avatar>
+                                <user-avatar :user="user.data" />
+                            </v-list-item-avatar>
+
+                            <!-- User information -->
+                            <v-list-item-content>
+                                <v-list-item-title>{{ user.data.name }}</v-list-item-title>
+                                <v-list-item-subtitle>test@example.org</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-divider class="my-3" />
+
+                        <!-- Account -->
+                        <v-list-item to="/account">
+                            <v-list-item-title>
+                                My account
+                            </v-list-item-title>
+                        </v-list-item>
+
+                        <!-- Settings -->
+                        <v-list-item to="/account/settings">
+                            <v-list-item-title>
+                                Settings
+                            </v-list-item-title>
+                        </v-list-item>
+
+                        <v-divider class="my-3" />
+
+                        <!-- Logout -->
+                        <v-list-item to="/account/logout">
+                            <v-list-item-title>
+                                Logout
+                            </v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </template>
+
+            <!-- Not logged in -->
+            <template v-else-if="user.isError()">
+                <v-btn text @click="openLogin">
+                    Login
+                </v-btn>
+            </template>
+        </div>
     </v-toolbar>
 </template>
 
@@ -51,8 +97,11 @@ import { User } from "@/api/models/User";
 import { EchoPromise } from "echofetch";
 import { ModalHandler } from "@/util/modal/ModalHandler";
 import LoginModal from "@/components/login/modals/LoginModal.vue";
+import UserAvatar from "@/components/user/UserAvatar.vue";
 
-@Component
+@Component({
+    components: { UserAvatar }
+})
 export default class NavigationBar extends Vue {
     /**
      * Should the drawer be open.
