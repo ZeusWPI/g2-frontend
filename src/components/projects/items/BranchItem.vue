aca6825a07fa7fa0f54efff7388743f06a1b388b
maartenvn <maartenvn.eu@gmail.com>
2020-07-13 16:39:17 +0200

    Add ability to link/unlink tags to a branch

--- src/components/projects/items/BranchItem.vue
+++ src/components/projects/items/BranchItem.vue
@@ -1,40 +1,80 @@
 <template>
-    <div class="branch">
-        <!-- Name -->
-        <div class="branch__name">
-            <a class="no-decoration" :href="branch.url">{{ branch.name }}</a>
-        </div>
+    <context-menu :items="contextItems">
+        <div class="branch">
+            <!-- Name -->
+            <div class="branch__name">
+                <a class="no-decoration" :href="branch.url">{{ branch.name }}</a>
+            </div>
 
-        <!-- Tags -->
-        <div class="branch__tags">
-            <project-tag v-for="(tag, index) of branch.tags" :key="index" :tag="tag" />
-        </div>
+            <!-- Tags -->
+            <div class="branch__tags">
+                <project-tag v-for="(tag, index) of branch.tags" :key="index" :tag="tag" />
+            </div>
 
-        <!-- Repository -->
-        <div class="branch__description text--secondary">
-            <b>Repository:</b>
+            <!-- Repository -->
+            <div class="branch__description text--secondary">
+                <b>Repository:</b>
 
-            <a class="no-decoration" :href="branch.repository.url">
-                {{ branch.repository.name }}
-            </a>
+                <a class="no-decoration" :href="branch.repository.url">
+                    {{ branch.repository.name }}
+                </a>
+            </div>
         </div>
-    </div>
+    </context-menu>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { Branch } from "@/api/models/Branch";
 import ProjectTag from "@/components/projects/ProjectTag.vue";
+import ContextMenu from "@/components/util/ContextMenu.vue";
+import { ContextMenuLink } from "@/types/ContextMenuLink";
+import { ModalHandler } from "@/util/modal/ModalHandler";
+import { Project } from "@/api/models/Project";
 
 @Component({
-    components: { ProjectTag }
+    components: { ContextMenu, ProjectTag }
 })
 export default class BranchItem extends Vue {
     /**
-     * Branch to display
+     * Project of the branch.
+     */
+    @Prop()
+    project: Project;
+
+    /**
+     * Branch to display.
      */
     @Prop()
     branch: Branch;
+
+    /**
+     * Get the context items to display inside the context menu.
+     */
+    get contextItems(): ContextMenuLink[] {
+        return [
+            {
+                text: "Edit tags",
+                icon: "mdi-tag-multiple-outline",
+                action: () =>
+                    // Open a modal for editing the tags for the given item.
+                    ModalHandler.open({
+                        component: () => import("../modals/EditTagsModal.vue"),
+                        componentPayload: {
+                            project: this.project,
+                            id: this.project.id,
+                            type: "branch"
+                        }
+                    })
+            },
+
+            {
+                text: "Feature item",
+                icon: "mdi-star-circle-outline",
+                action: () => console.log("HERE")
+            }
+        ];
+    }
 }
 </script>
 
