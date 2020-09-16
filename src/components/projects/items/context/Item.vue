d0bdbbb762a5a1279c9cb806f89952fe7d3a9da5
maartenvn <maartenvn.eu@gmail.com>
2020-09-16 16:08:33 +0200

    Fix incorrect item class name

--- src/components/projects/items/context/Item.vue
+++ src/components/projects/items/context/Item.vue
@@ -9,7 +9,7 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
-export default class VItem extends Vue {
+export default class Item extends Vue {
     /**
      * If the item should change color when hovering.
      */
