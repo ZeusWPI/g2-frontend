1e940f9709ffc38ef620e4804144c4c56e18a83e
mcbloch <maxime.bloch@ugent.be>
2020-12-10 01:38:41 +0100

    fix session cookie not send

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
 import vuetify from "./plugins/vuetify";
+import axios from "axios";
 
 import "./plugins/i18n";
 import "./assets/scss/main.scss";
 
 import "./assets/scss/main.scss";
+
+axios.defaults.withCredentials = true;
 
 new Vue({
     router,
